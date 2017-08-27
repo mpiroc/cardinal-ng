9b8d4e232336ca42084770844f9f73ebb0e1ddaf
Matthew Pirocchi <matthew.pirocchi@gmail.com>
2017-08-27 15:45:43 -0400

    Add response region to review route.

--- src/app/components/review-deck-route/review-deck-route.component.ts
+++ src/app/components/review-deck-route/review-deck-route.component.ts
@@ -11,6 +11,11 @@ import {
 import { IState, isListening } from '../../redux/state';
 
 
+interface IResponse {
+  grade: number;
+  text: string;
+}
+
 @WithSubStore({
   basePathMethodName: "getBasePath",
   localReducer: DeckCardListReducer.reducer,
@@ -23,6 +28,33 @@ import { IState, isListening } from '../../redux/state';
 export class ReviewDeckRouteComponent implements OnInit {
   private deck: IUserDeck;
   private card$: Observable<IDeckCard>;
+  private selectedResponse: IResponse;
+  private responses: IResponse[] = [
+    {
+      grade: 0,
+      text: "I completely forgot the card.",
+    },
+    {
+      grade: 1,
+      text: "I forgot most of the card.",
+    },
+    {
+      grade: 2,
+      text: "I forgot some of the card.",
+    },
+    {
+      grade: 3,
+      text: "I remembered the card with much difficulty.",
+    },
+    {
+      grade: 4,
+      text: "I remembered the card with some difficulty.",
+    },
+    {
+      grade: 5,
+      text: "I easily remembered the card.",
+    },
+ ]
 
   @select(["data"])
   deckCards$: Observable<Map<string, IDeckCard>>;
