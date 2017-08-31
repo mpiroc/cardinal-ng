85e806a980e97f9ab0aa8f72d1df1bb25612823d
Matthew Pirocchi <matthew.pirocchi@gmail.com>
2017-08-31 19:19:42 -0400

    Debounce isLoading before showing progress spinner.

--- src/app/components/loading-spinner/loading-spinner.component.ts
+++ src/app/components/loading-spinner/loading-spinner.component.ts
@@ -1,10 +1,23 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/timer';
+import 'rxjs/add/operator/debounce';
+
 @Component({
   selector: 'cardinal-loading-spinner',
   templateUrl: './loading-spinner.component.html',
   styleUrls: [ './loading-spinner.component.css' ],
 })
-export class LoadingSpinnerComponent {
+export class LoadingSpinnerComponent implements OnInit {
   @Input()
-  isLoading: boolean;
+  isLoading$: Observable<boolean>;
+
+  isLoadingDebounced$: Observable<boolean>;
+
+  ngOnInit() {
+    this.isLoadingDebounced$ = this.isLoading$
+      .debounce(isLoading => isLoading ?
+        Observable.timer(300) :
+        Observable.timer(0));
+  }
 }
