8afbf820ec37a6f42dc06104f781d1d33ecbf31a
Matthew Pirocchi <matthew.pirocchi@gmail.com>
2017-09-05 19:29:07 -0400

    Disable Sign Up button while email and password are not valid.

--- src/app/components/sign-up/sign-up.component.ts
+++ src/app/components/sign-up/sign-up.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { NgRedux } from '@angular-redux/store';
 import { FormControl, Validators } from '@angular/forms';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/Observable/combineLatest';
+import 'rxjs/add/operator/map';
 import { AuthService } from '../../services/firebase/auth.service';
 import {
   signUpSetEmail,
@@ -23,7 +26,12 @@ export class SignUpComponent {
     Validators.required,
     Validators.minLength(12),
     Validators.pattern(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).*$/),
-  ])
+  ]);
+
+  private isValid$: Observable<boolean> = Observable.combineLatest(
+      this.emailFormControl.statusChanges,
+      this.passwordFormControl.statusChanges,
+    ).map(results => results[0] === 'VALID' && results[1] === 'VALID');
 
   constructor(private authService: AuthService, private ngRedux: NgRedux<IState>) {
   }
