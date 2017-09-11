11ce8661e2e68293060fdbeac025f4ca4ba0a21d
Matthew Pirocchi <matthew.pirocchi@gmail.com>
2017-09-10 23:09:03 -0400

    Use separate database in production.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,12 +2,12 @@ export const environment = {
   production: true,
   // TODO: Create separate production database.
   firebase: {
-    apiKey: 'AIzaSyBCjcVowCuBu3nuXLRsNCAhR56QcZm4o9M',
-    authDomain: 'cardinal-ng.firebaseapp.com',
-    databaseURL: 'https://cardinal-ng.firebaseio.com',
-    projectId: 'cardinal-ng',
-    storageBucket: 'cardinal-ng.appspot.com',
-    messagingSenderId: '956312128006'
+    apiKey: "AIzaSyBBAOaYYEETdE3mkjgjs0_nrCJkveWugpo",
+    authDomain: "cardinal-prod.firebaseapp.com",
+    databaseURL: "https://cardinal-prod.firebaseio.com",
+    projectId: "cardinal-prod",
+    storageBucket: "cardinal-prod.appspot.com",
+    messagingSenderId: "367381328845"
   },
   routing: {
   },
