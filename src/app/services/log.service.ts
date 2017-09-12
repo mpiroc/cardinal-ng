89554ff5d12fccdc7d4a113e2149664eaf33240b
Matthew Pirocchi <matthew.pirocchi@gmail.com>
2017-09-12 12:58:32 -0400

    Remove log levels below error from log service.

--- src/app/services/log.service.ts
+++ src/app/services/log.service.ts
@@ -6,46 +6,14 @@ import { environment } from '../../environments/environment';
 
 @Injectable()
 export class LogService {
-  private infoSubject: Subject<any> = new Subject<any>();
-  private debugSubject: Subject<any> = new Subject<string>();
-  private logSubject: Subject<any> = new Subject<any>();
   private errorSubject: Subject<any> = new Subject<any>();
 
-  get info$(): Observable<any> {
-    return this.infoSubject.asObservable();
-  }
-
-  get debug$(): Observable<any> {
-    return this.infoSubject.asObservable();
-  }
-
-  get log$(): Observable<any> {
-    return this.infoSubject.asObservable();
-  }
-
   get error$(): Observable<any> {
-    return this.infoSubject.asObservable();
+    return this.errorSubject.asObservable();
   }
 
   constructor() {
-    if (!environment.production) {
-      this.info$.subscribe(message => console.info(message));
-      this.debug$.subscribe(message => console.debug(message));
-      this.log$.subscribe(message => console.log(message));
-      this.error$.subscribe(message => console.error(message));
-    }
-  }
-
-  info(message: any): void {
-    this.infoSubject.next(message);
-  }
-
-  debug(message: any): void {
-    this.debugSubject.next(message);
-  }
-
-  log(message: any): void {
-    this.logSubject.next(message);
+    this.error$.subscribe(message => console.error(message));
   }
 
   error(message: any): void {
