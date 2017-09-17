25d4ace45d12670cb4f4f2f13e7f36b60e565612
Matthew Pirocchi <matthew.pirocchi@gmail.com>
2017-09-17 18:41:59 -0400

    Switch to automation-friendly test settings.

--- karma.conf.js
+++ karma.conf.js
@@ -27,9 +27,9 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
-    singleRun: false
-    //browsers: ['ChromeHeadless'],
-    //singleRun: true
+    //browsers: ['Chrome'],
+    //singleRun: false
+    browsers: ['ChromeHeadless'],
+    singleRun: true
   });
 };
